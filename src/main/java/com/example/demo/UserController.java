d20071cb7f4317b702537f8ce9475c1ce8e5048f
up1 <somkiat.p@gmail.com>
2020-08-05 10:56:22 +0700

    Step 2

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -7,8 +7,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     @GetMapping("/users")
-    public String getAllUser() {
-        return "Get All User";
+    public UsersResponse[] getAllUser() {
+        UsersResponse[] usersResponses = new UsersResponse[2];
+        usersResponses[0] = new UsersResponse();
+        usersResponses[1] = new UsersResponse();
+        return usersResponses;
     }
 
 }
---
+++ src/main/java/com/example/demo/UsersResponse.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+public class UsersResponse {
+    private int id;
+    private String name;
+
+    // POJO (Plain Old Java Object)
+    // Getter/Setter methods
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}

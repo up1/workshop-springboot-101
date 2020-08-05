4c0fc656b6c4881673d8d4cc6572db8d65c9a9ae
up1 <somkiat.p@gmail.com>
2020-08-05 13:53:10 +0700

    Working with json

--- src/main/java/com/example/demo/PagingResponse.java
+++ src/main/java/com/example/demo/PagingResponse.java
@@ -1,10 +1,18 @@
 package com.example.demo;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
 import java.util.List;
 
+@JsonPropertyOrder({"page", "item_per_page", "users"})
 public class PagingResponse {
+    @JsonProperty("users")
     private List<UsersResponse> usersResponseList;
+
     private int page;
+
+    @JsonProperty("item_per_page")
     private int itemPerPage;
 
     public PagingResponse(int page, int itemPerPage) {

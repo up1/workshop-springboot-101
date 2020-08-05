9e669d9c7ed6b7b3ad9cad7b4ebdc431aa1dedd0
up1 <somkiat.p@gmail.com>
2020-08-05 10:45:43 +0700

    Step 1

---
+++ src/main/java/com/example/demo/UserController.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/users")
+    public String getAllUser() {
+        return "Get All User";
+    }
+
+}

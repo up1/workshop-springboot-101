a4d86d9ea6d8ca52efed4d2a60782063a3888e8f
up1 <somkiat.p@gmail.com>
2020-08-05 15:47:04 +0700

    Get all user from repository

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -31,9 +31,12 @@ public class UserController {
 
         PagingResponse pagingResponse = new PagingResponse(page, itemPerPage);
         List<UsersResponse> usersResponseList = new ArrayList<>();
-        usersResponseList.add(new UsersResponse(1, "User 1"));
-        usersResponseList.add(new UsersResponse(2, "User 2"));
-        usersResponseList.add(new UsersResponse(3, "User 3"));
+
+        Iterable<User> users = userRepository.findAll();
+        for (User user: users) {
+            usersResponseList.add(new UsersResponse(user.getId(), user.getName()));
+        }
+
         pagingResponse.setUsersResponse(usersResponseList);
         return pagingResponse;
     }

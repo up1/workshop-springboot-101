c573c7114a510412d8d02fd29dc4420fd8b34514
up1 <somkiat.p@gmail.com>
2020-08-05 11:45:16 +0700

    Step get user by id

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -1,17 +1,27 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class UserController {
 
     @GetMapping("/users")
-    public UsersResponse[] getAllUser() {
-        UsersResponse[] usersResponses = new UsersResponse[2];
-        usersResponses[0] = new UsersResponse(1, "User 1");
-        usersResponses[1] = new UsersResponse(2, "User 2");
-        return usersResponses;
+    public List<UsersResponse> getAllUser() {
+        List<UsersResponse> usersResponseList = new ArrayList<>();
+        usersResponseList.add(new UsersResponse(1, "User 1"));
+        usersResponseList.add(new UsersResponse(2, "User 2"));
+        usersResponseList.add(new UsersResponse(3, "User 3"));
+        return usersResponseList;
+    }
+
+    @GetMapping("/users/{id}")
+    public UsersResponse getUserById(@PathVariable int id) {
+        return new UsersResponse(id, "User " + id);
     }
 
 }
